60f81356546013a9409456e02db3e7206f132746
Mario Wenzel <maweki@gmail.com>
2016-04-02 14:30:33 +0200

    Convert from EcmaScript6

--- srv.js
+++ srv.js
@@ -3,10 +3,23 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+var babel = require("babel-core");
 
 var staticfolder = __dirname + '/static';
 var vendfolder = __dirname + '/node_modules';
 
+// Generate ES5
+var generate_es5 = function(filename) {
+  var options = {presets: ["es2015"]}
+      code = babel.transformFileSync(
+              staticfolder + '/' + filename + '.es6.js',
+              options
+             ).code;
+  fs.writeFileSync(staticfolder + '/' + filename + '.js', code);
+}
+generate_es5('digispt');
+
+// Static Data
 app.use('/vend', express.static(vendfolder));
 app.use(express.static(staticfolder));
 
@@ -22,6 +35,7 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+// Connections
 var data = { last: "{}" };
 data.last = JSON.parse(fs.readFileSync(__dirname + '/data.default.json'));
 var datafile = __dirname + '/data.json'
